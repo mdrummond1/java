68fdd4475e0df5b7b10d7aa690c31e4b08e0f3f0
Matthew Drummond <mattatatter@gmail.com>
2020-01-28 09:09:00 -0600

    first java programs

---
+++ Salary.java
@@ -0,0 +1,11 @@
+public class Salary {                         
+        public static void main (String [] args) { 
+           int wage;                          
+     
+           wage = 20;
+     
+           System.out.print("Salary is ");         
+           System.out.println(wage * 40 * 50);     
+        } 
+     } 
+     
