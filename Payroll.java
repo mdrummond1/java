9a823da2e31504c6fb2c94a201755a01e3785467
Matthew Drummond <mattatatter@gmail.com>
2020-01-31 17:15:53 -0600

    deleted 'first'.

--- Payroll.java
+++ Payroll.java
@@ -1,7 +1,32 @@
 //for payroll lab from zybooks
+//Matthew Drummond
+import java.util.Scanner;
 public class Payroll {
     public static void main(String[] args) {
-        //print payroll
-        System.out.println("heya");
+        Scanner scnr = new Scanner(System.in);
+        String firstName;
+        String lastName;
+        int hoursWorked;
+        double payRate;
+        double takeHomePay;
+
+        //prompt user and get input
+        System.out.println("Enter your first name:");
+        firstName = scnr.next();
+
+        System.out.println("Enter your last name:");
+        lastName = scnr.next();
+
+        System.out.println("Enter your hours worked:");
+        hoursWorked = scnr.nextInt();
+
+        System.out.println("Enter your pay rate:");
+        payRate = scnr.nextDouble();
+
+        takeHomePay = hoursWorked * payRate;
+        
+        System.out.println("" + firstName + " " + lastName + ", your pay is $" + takeHomePay);
+        scnr.close();
+        
     }
 }
